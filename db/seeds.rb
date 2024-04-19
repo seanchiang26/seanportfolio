3f911f9f9ccd21ad7182c1b5dd632be11eb932bf
Sean Chiang <seanchiang226@gmail.com>
2024-04-19 02:18:46 -0700

    Set up doorkeeper

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Doorkeeper::Application.count.zero?
+  Doorkeeper::Application.create!(name: 'Web Client', redirect_uri: '', scopes: '')
+  Doorkeeper::Application.create!(name: 'iOS Client', redirect_uri: '', scopes: '')
+  Doorkeeper::Application.create!(name: 'Android Client', redirect_uri: '', scopes: '')
+  Doorkeeper::Application.create!(name: 'React', redirect_uri: '', scopes: '')
+end
