0d0368b4f82dc597e58f3b5cc7711545cf231bf4
Sean Chiang <seanchiang226@gmail.com>
2024-04-29 21:36:37 -0700

    style projects.show a bit

--- app/frontend/pages/Projects.Show.jsx
+++ app/frontend/pages/Projects.Show.jsx
@@ -1,4 +1,15 @@
-import { Button, Container, Stack } from "@mantine/core";
+import {
+  Text,
+  Container,
+  Divider,
+  Stack,
+  Title,
+  Group,
+  Pill,
+  SimpleGrid,
+  Image,
+  Paper,
+} from "@mantine/core";
 import { router, Link, usePage } from "@inertiajs/react";
 
 export default function ProjectsShow({ project }) {
@@ -6,24 +17,28 @@ export default function ProjectsShow({ project }) {
 
   return (
     <Container size="md">
-      <h1>{project.title}</h1>
-      <p>{project.body}</p>
-      {auth && auth.role == "admin" ? (
-        <Stack>
-          <Link href={`/projects/${project.id}/edit`}>Edit</Link>
-          <div>
-            <Button
-              onClick={() => {
-                router.delete(`/projects/${project.id}`);
-              }}
-            >
-              Delete
-            </Button>
-          </div>
-        </Stack>
-      ) : (
-        <></>
-      )}
+      <Stack gap="lg">
+        <Title order={2}>{project.title}</Title>
+        <Title order={4}>{project.one_liner}</Title>
+        <Group>
+          {project.tag_list.map(tag => (
+            <Pill key={project.id + tag}>{tag}</Pill>
+          ))}
+        </Group>
+
+        <Divider />
+
+        <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="xl">
+          <Text>{project.body}</Text>
+          <Image
+            fit="contain"
+            radius="md"
+            src={
+              "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80"
+            }
+          />
+        </SimpleGrid>
+      </Stack>
     </Container>
   );
 }
