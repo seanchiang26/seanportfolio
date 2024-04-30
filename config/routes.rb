2d73efbfeed47d9f9483003bb9da6d633358e6de
Sean Chiang <seanchiang226@gmail.com>
2024-04-30 04:25:31 -0700

    Remove messages route (maybe I'll reimplement it later)

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
   end
 
   resources :projects
-  resources :messages
 
   draw :api
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
