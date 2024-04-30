5b3abbaf419eaa84dfdaa12b0dff629e1c988a80
Sean Chiang <seanchiang226@gmail.com>
2024-04-30 04:33:42 -0700

    Update contact form

--- app/frontend/components/contact/ContactForm.jsx
+++ app/frontend/components/contact/ContactForm.jsx
@@ -22,23 +22,6 @@ import { ContactIconsList } from "./ContactIcons";
 import classes from "./ContactForm.module.css";
 
 export function ContactForm() {
-  const form = useForm({
-    mode: "uncontrolled",
-    initialValues: {
-      message: {
-        email: "",
-        name: "",
-        body: "",
-      },
-    },
-
-    validate: {
-      message: {
-        email: value => (/^\S+@\S+$/.test(value) ? null : "invalid email"),
-      },
-    },
-  });
-
   return (
     <Container id="contact" size="md" className={classes.wrapper}>
       <SimpleGrid cols={{ base: 1, sm: 2 }} spacing={50}>
@@ -86,60 +69,82 @@ export function ContactForm() {
             </a>
           </Group>
         </div>
-        <div className={classes.form}>
-          <form
-            onSubmit={form.onSubmit(values =>
-              axios
-                .post("/messages", values)
-                .then(res =>
-                  res.status == 201
-                    ? notifications.show({
-                        color: "blue",
-                        title: "Notice",
-                        message: "Message received!",
-                      })
-                    : notifications.show({
-                        color: "red",
-                        title: "Alert",
-                        message: "Something went wrong, try again later!",
-                      }),
-                ),
-            )}
-          >
-            <TextInput
-              label="email"
-              placeholder="your@email.com"
-              required
-              classNames={{ input: classes.input, label: classes.inputLabel }}
-              {...form.getInputProps("message.email")}
-            />
-            <TextInput
-              label="name"
-              placeholder="john doe"
-              mt="md"
-              classNames={{ input: classes.input, label: classes.inputLabel }}
-              {...form.getInputProps("message.name")}
-            />
-            <Textarea
-              required
-              label="your message"
-              placeholder="i want to be your friend"
-              minRows={4}
-              maxRows={4}
-              autosize
-              mt="md"
-              classNames={{ input: classes.input, label: classes.inputLabel }}
-              {...form.getInputProps("message.body")}
-            />
 
-            <Group justify="flex-end" mt="md">
-              <Button className={classes.control} type="submit">
-                send message
-              </Button>
-            </Group>
-          </form>
-        </div>
+        {/* <ContactFormBody /> */}
       </SimpleGrid>
     </Container>
+  );
+}
+
+function ContactFormBody() {
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      message: {
+        email: "",
+        name: "",
+        body: "",
+      },
+    },
+
+    validate: {
+      message: {
+        email: value => (/^\S+@\S+$/.test(value) ? null : "invalid email"),
+      },
+    },
+  });
+
+  return (
+    <div className={classes.form}>
+      <form
+        onSubmit={form.onSubmit(values =>
+          axios.post("/messages", values).then(res =>
+            res.status == 201
+              ? notifications.show({
+                  color: "blue",
+                  title: "Notice",
+                  message: "Message received!",
+                })
+              : notifications.show({
+                  color: "red",
+                  title: "Alert",
+                  message: "Something went wrong, try again later!",
+                }),
+          ),
+        )}
+      >
+        <TextInput
+          label="email"
+          placeholder="your@email.com"
+          required
+          classNames={{ input: classes.input, label: classes.inputLabel }}
+          {...form.getInputProps("message.email")}
+        />
+        <TextInput
+          label="name"
+          placeholder="john doe"
+          mt="md"
+          classNames={{ input: classes.input, label: classes.inputLabel }}
+          {...form.getInputProps("message.name")}
+        />
+        <Textarea
+          required
+          label="your message"
+          placeholder="i want to be your friend"
+          minRows={4}
+          maxRows={4}
+          autosize
+          mt="md"
+          classNames={{ input: classes.input, label: classes.inputLabel }}
+          {...form.getInputProps("message.body")}
+        />
+
+        <Group justify="flex-end" mt="md">
+          <Button className={classes.control} type="submit">
+            send message
+          </Button>
+        </Group>
+      </form>
+    </div>
   );
 }
