fd260f843beb9a2fb16bc968ad57a3740362c46a
Sean Chiang <seanchiang226@gmail.com>
2024-04-30 05:00:27 -0700

    Update card images

--- app/frontend/components/homepage/projects-carousel/ProjectsCard.jsx
+++ app/frontend/components/homepage/projects-carousel/ProjectsCard.jsx
@@ -1,31 +1,35 @@
-import { Paper, Title, Button, Text } from "@mantine/core";
+import { Card, Title, Button, Text, Overlay } from "@mantine/core";
 import { Link } from "@inertiajs/react";
 import classes from "./ProjectsCarousel.module.css";
 
 export function ProjectsCard({ title, one_liner, slug }) {
   return (
-    <Paper
+    <Card
       shadow="md"
-      p="xl"
       radius="md"
+      p="xl"
       style={{
-        backgroundImage: `url("https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80")`,
+        backgroundImage: `url("/images/diehard_ss.png")`,
       }}
       className={classes.card}
     >
-      <div>
-        <Title order={2} className={classes.title}>
-          {title}
-        </Title>
-        <Text className={classes.one_liner} size="xs" mt="xs">
-          {one_liner}
-        </Text>
+      <Overlay className={classes.overlay} opacity={0.55} zIndex={0} />
+
+      <div className={classes.content}>
+        <div>
+          <Title order={2} className={classes.title}>
+            {title}
+          </Title>
+          <Text className={classes.one_liner} size="xs" mt="xs">
+            {one_liner}
+          </Text>
+        </div>
+        <Link href={`/projects/${slug}`}>
+          <Button variant="white" color="dark" className={classes.action}>
+            learn more
+          </Button>
+        </Link>
       </div>
-      <Link href={`/projects/${slug}`}>
-        <Button variant="white" color="dark">
-          learn more
-        </Button>
-      </Link>
-    </Paper>
+    </Card>
   );
 }
--- app/frontend/components/projects/ProjectCard.jsx
+++ app/frontend/components/projects/ProjectCard.jsx
@@ -3,8 +3,7 @@ import { Link } from "@inertiajs/react";
 import classes from "./ProjectCard.module.css";
 
 export function ProjectCard({ project }) {
-  const image =
-    "https://images.unsplash.com/photo-1596394516093-501ba68a0ba6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80";
+  const image = "/images/diehard_ss.png";
 
   return (
     <Card
