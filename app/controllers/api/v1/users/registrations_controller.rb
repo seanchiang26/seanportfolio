0ac3d27b425ac89e0561635be8b25e6b24db44ab
Sean Chiang <seanchiang226@gmail.com>
2024-05-01 22:23:07 -0700

    Disable user registration through API

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -8,22 +8,22 @@ module Api
 
         include DoorkeeperRegisterable
 
-        def create
-          client_app = Doorkeeper::Application.find_by(uid: params[:client_id])
-          unless client_app
-            return render json: { error: I18n.t('doorkeeper.errors.messages.invalid_client') },
-                          status: :unauthorized
-          end
+        # def create
+        #   client_app = Doorkeeper::Application.find_by(uid: params[:client_id])
+        #   unless client_app
+        #     return render json: { error: I18n.t('doorkeeper.errors.messages.invalid_client') },
+        #                   status: :unauthorized
+        #   end
 
-          allowed_params = user_params.except(:client_id)
-          user = User.new(allowed_params)
+        #   allowed_params = user_params.except(:client_id)
+        #   user = User.new(allowed_params)
 
-          if user.save
-            render json: render_user(user, client_app), status: :ok
-          else
-            render json: { errors: user.errors }, status: :unprocessable_entity
-          end
-        end
+        #   if user.save
+        #     render json: render_user(user, client_app), status: :ok
+        #   else
+        #     render json: { errors: user.errors }, status: :unprocessable_entity
+        #   end
+        # end
 
         private
 
