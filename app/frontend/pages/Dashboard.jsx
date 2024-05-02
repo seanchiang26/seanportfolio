ff0e89fb35858d65f737fabf4523388c81620e22
Sean Chiang <seanchiang226@gmail.com>
2024-05-02 03:32:13 -0700

    Add TODOs

--- app/frontend/pages/Dashboard.jsx
+++ app/frontend/pages/Dashboard.jsx
@@ -1,6 +1,16 @@
 import { router } from "@inertiajs/react";
 import { Button, Container } from "@mantine/core";
 
+/**
+ * TODO:
+ *   - Manage projects on dashboard
+ *   - Format forms
+ *   - Proper login screen
+ *   - Dynamic head
+ *   - Implement SSR
+ *   - Mailers? so i can implement the messaging box
+ */
+
 export default function Dashboard() {
   return (
     <Container size="md">
