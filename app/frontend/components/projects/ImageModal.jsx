f46cd44a4899fe358fce623c59cd3ca552220806
Sean Chiang <seanchiang226@gmail.com>
2024-05-01 22:17:32 -0700

    Update image modal

--- app/frontend/components/projects/ImageModal.jsx
+++ app/frontend/components/projects/ImageModal.jsx
@@ -1,18 +1,20 @@
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, UnstyledButton, Image } from "@mantine/core";
+import { Box, Modal, UnstyledButton, Image } from "@mantine/core";
 
 export function ImageModal({ image_url }) {
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <>
-      <Modal opened={opened} size="xl" onClose={close} title="Project Image">
+      <Modal opened={opened} size="xl" onClose={close} title="see image">
         <Image fit="contain" radius="md" src={image_url} />
       </Modal>
 
-      <UnstyledButton onClick={open}>
-        <Image fit="contain" radius="md" src={image_url} />
-      </UnstyledButton>
+      <Box>
+        <UnstyledButton onClick={open}>
+          <Image fit="contain" radius="md" src={image_url} />
+        </UnstyledButton>
+      </Box>
     </>
   );
 }
